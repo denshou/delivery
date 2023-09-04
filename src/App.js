081ae3c33f141d19a525c0b8dc6684cf22602d47
denshou <132555762+denshou@users.noreply.github.com>
2023-09-04 15:56:45 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { useState } from "react";
-// import Modal from "./components/Modal";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-
-  // const getModalOpen = (data) => { // data = isOpen
-
-  // }
 
   return (
     <div className="App">
       <Header isOpen={isOpen} setIsOpen={setIsOpen} />
       <Body isOpen={isOpen} setIsOpen={setIsOpen} />
-      {/* {isOpen && <Modal isOpen={isOpen} setIsOpen={setIsOpen}/>} */}
     </div>
   );
 }
